c27a26218e946d1edcbdbb49cb8c25cd2790fcd2
tvhfrks12678 <tvhfrks12678@gmail.com>
2026-01-31 18:06:23 +0900

    feat(header): implement comic-text for main title

--- src/components/RhymeQuizHeader.tsx
+++ src/components/RhymeQuizHeader.tsx
@@ -1,10 +1,13 @@
+import { ComicText } from "@/components/ui/comic-text"
+
 export default function RhymeQuizHeader() {
     return (
         <header>
+            <ComicText>Rhyme Quiz</ComicText>
             {/* <h1>Rhyme Quiz</h1> */}
-            <h1 className="scroll-m-20 text-center text-6xl font-extrabold tracking-tight text-balance">
+            {/* <h1 className="scroll-m-20 text-center text-6xl font-extrabold tracking-tight text-balance">
                 🎤Rhyme Quiz
-            </h1>
+            </h1> */}     
         </header>
     )
 }
