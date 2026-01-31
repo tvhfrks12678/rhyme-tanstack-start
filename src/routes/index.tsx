c1e25ad2707991a7bca6de58cd40a2547a1e9a73
tvhfrks12678 <tvhfrks12678@gmail.com>
2026-01-31 20:26:06 +0900

    feat(ui): add field component from shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from '@/components/ui/label'
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+  FieldTitle,
+} from "@/components/ui/field"
 import { createFileRoute } from '@tanstack/react-router'
 // import {
 //   Zap,
@@ -24,6 +32,50 @@ function App() {
         <Checkbox id="terms" />
         <Label htmlFor="terms">Accept terms and conditions</Label>
       </div>
+
+
+
+
+
+      <FieldGroup className="max-w-sm">
+        {/* <Field orientation="horizontal">
+          <Checkbox id="terms-checkbox" name="terms-checkbox" />
+          <Label htmlFor="terms-checkbox">Accept terms and conditions</Label>
+        </Field>
+        <Field orientation="horizontal">
+          <Checkbox
+            id="terms-checkbox-2"
+            name="terms-checkbox-2"
+            defaultChecked
+          />
+          <FieldContent>
+            <FieldLabel htmlFor="terms-checkbox-2">
+              Accept terms and conditions
+            </FieldLabel>
+            <FieldDescription>
+              By clicking this checkbox, you agree to the terms.
+            </FieldDescription>
+          </FieldContent>
+        </Field>
+        <Field orientation="horizontal" data-disabled>
+          <Checkbox id="toggle-checkbox" name="toggle-checkbox" disabled />
+          <FieldLabel htmlFor="toggle-checkbox">Enable notifications</FieldLabel>
+        </Field> */}
+        <FieldLabel>
+          <Field orientation="horizontal">
+            <Checkbox id="toggle-checkbox-2" name="toggle-checkbox-2" />
+            <FieldContent>
+              <FieldTitle>Enable notifications</FieldTitle>
+              <FieldDescription>
+                You can enable or disable notifications at any time.
+              </FieldDescription>
+            </FieldContent>
+          </Field>
+        </FieldLabel>
+      </FieldGroup>
+
+
+
 
     </div>
   )
